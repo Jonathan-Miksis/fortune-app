1057faf58593b053a969b3250512423aae448909
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-10 12:33:56 -0400

    post one random fortune to user when refreshing the page

--- app/controllers/my_exaples_controller.rb
+++ app/controllers/my_exaples_controller.rb
@@ -1,7 +1,8 @@
 class MyExaplesController < ApplicationController
 
   def fortune_action
-    render json: {}
+    fortune = ["You will get a new promotion", "You will win the lottery", "You will win a vacation to the islands"]
+    render json: {message: fortune.shuffle.last}
   end
 
 end
